765a681730ae31fde1ed1481aab0550eb08eccbf
Amit Kumar <amit.kumar@bluestacks.com>
2020-12-31 03:24:42 +0530

    wait infinite until image is pulled

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -11,7 +11,7 @@ dockerpath=amitrhe/ml-microservice
 # Run the Docker Hub container with kubernetes
 kubectl create deployment udacity-ml --image=amitrhe/ml-microservice --port=80
 # wait till pods become ready
-kubectl wait --for=condition=ready pod -l app=udacity-ml
+kubectl wait --timeout=-1s --for=condition=ready pod -l app=udacity-ml
 
 # Step 3:
 # List kubernetes pods
@@ -19,4 +19,3 @@ kubectl get pods
 # Step 4:
 # Forward the container port to a host
 kubectl port-forward deployment/udacity-ml 8000:80
-
